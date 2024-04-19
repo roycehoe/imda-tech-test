4145f6c627a958b884765bca24764c837a5dc996
roycehoe <royceinbox@hotmail.com>
2024-04-19 21:50:22 +0800

    feat: Create mantenance menu

---
+++ display.py
@@ -0,0 +1,124 @@
+from dataclasses import dataclass
+
+PROMPT_INPUT = "Input: "
+START_MENU_DISPLAY = """
+-------------------------------------
+Impossibly Mighty Detergent Activator
+-------------------------------------
+
+Hello there! Thanks for choosing IMDA for your washing needs.
+How may we assist you today?
+
+[1] Start
+[2] Mantenance
+[3] Exit
+"""
+
+EXIT_DISPLAY = """Goodbye"""
+MACHINE_MENU_DISPLAY = """
+Please select one of the following options:
+
+[1] Insert coins
+[2] Select wash
+
+Otherwise, press [0] to go back"""
+
+INSERT_COINS_MENU_DISPLAY = """
+Please choose one of the following coins to insert:
+
+[1] 10c
+[2] 20c
+[3] 50c
+[4] $1
+
+Otherwise, press [5] to go back"""
+
+SELECT_WASH_MENU_DISPLAY = """
+Please select one of the following washes:
+
+[1] Quick Wash: 10 mins - $2.00
+[2] Mild Wash: 30 mins - $2.50
+[3] Medium Wash: 45 mins - $4.20
+[4] Heavy Wash: 1 hour - $6.00
+
+Otherwise, press [0] to go back"""
+
+INSUFFICIENT_FUNDS_DISPLAY = """Sorry, it seems you have insufficient funds to do that.
+
+Please select a cheaper wash. Otherwise, try topping up your wallet"""
+
+START_WASH_DISPLAY = """
+Door locked. Wash starting...
+"""
+
+END_WASH_DISPLAY = """
+Wash finished! The door is now unlocked. Please don't forget to take your clothes!
+"""
+
+MAINTENANCE_MENU = """
+(Admin) What would you like to do?
+
+[1] Display statistics
+[2] Reset statistics
+
+Otherwise, press [0] to go back"""
+
+
+STATISTICS_RESET_DISPLAY = """
+All statistics has been reset
+"""
+
+INVALID_SELECTION_DISPLAY = """
+You have selected an invalid option. Please try again
+"""
+
+
+DEFAULT_TOTAL_TIME_SWITCHED_ON_HOURS = 0
+DEFAULT_MONEY_EARNED = 0.00
+
+@dataclass
+class WashingMachineState:
+    balance: float = 0.00
+
+    def topup_balance(self, money: float):
+        self.balance += money
+
+
+@dataclass
+class WashingMachineStatistics:
+    total_time_switched_on_hours: int = DEFAULT_TOTAL_TIME_SWITCHED_ON_HOURS
+    money_earned: float = DEFAULT_MONEY_EARNED
+
+    def reset(self):
+        self.total_time_switched_on_hours = DEFAULT_TOTAL_TIME_SWITCHED_ON_HOURS
+        self.money_earned = DEFAULT_MONEY_EARNED
+
+    def __str__(self):
+        return f"""
+Total time switched on: {self.total_time_switched_on_hours} hours
+Balance: ${self.money_earned:.2f}"""
+
+
+def show_wallet_balance(balance: float) -> None:
+    print(f"Current balance: ${balance}")
+
+
+def show_statistics(machine_statistics: WashingMachineStatistics) -> None:
+    print(machine_statistics)
+
+
+def show_insert_coin_success_message(coin_value: float) -> None:
+    print(f"Ding! ${coin_value} successfully added to your wallet")
+
+
+def show_refund_excess_message(excess_amount: float) -> None:
+    print(f"Clonk clonk. ${excess_amount} refunded")
+
+
+def show_washing_job_progress(
+    progress_percentage: float, remaining_time_minutes: int
+) -> None:
+    print(
+        f"""Current progress: {progress_percentage * 100:.0f}%
+Remaining time: {remaining_time_minutes} mins"""
+    )
--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from enum import Enum, auto
 from display import (
     EXIT_DISPLAY,
     INVALID_SELECTION_DISPLAY,
+    MACHINE_MENU_DISPLAY,
+    MAINTENANCE_MENU,
     PROMPT_INPUT,
     START_MENU_DISPLAY,
+    STATISTICS_RESET_DISPLAY,
+    WashingMachineState,
+    WashingMachineStatistics,
+    show_statistics,
 )
 
 DEFAULT_WALLET_BALANCE = 0.00
@@ -21,7 +27,7 @@ class StartMenuSelection(Enum):
     EXIT = auto()
 
 
-class UseMachineMenuSelection(Enum):
+class MachineMenuSelection(Enum):
     INSERT_COINS = auto()
     SELECT_WASH = auto()
     GO_BACK = auto()
@@ -70,8 +76,74 @@ def get_start_menu_input() -> StartMenuSelection:
             print(INVALID_SELECTION_DISPLAY)
 
 
+def get_maintenance_menu_selection() -> MaintenanceMenuSelection:
+    maintenance_menu_selection = input(PROMPT_INPUT)
+    if maintenance_menu_selection == "1":
+        return MaintenanceMenuSelection.DISPLAY_STATISTICS
+    if maintenance_menu_selection == "2":
+        return MaintenanceMenuSelection.RESET_STATISTICS
+    if maintenance_menu_selection == "0":
+        return MaintenanceMenuSelection.GO_BACK
+    raise InvalidMenuSelectionError
+
+
+def get_maintenance_menu_input() -> MaintenanceMenuSelection:
+    while True:
+        try:
+            print(MAINTENANCE_MENU)
+            maintenance_menu_selection = get_maintenance_menu_selection()
+            return maintenance_menu_selection
+        except InvalidMenuSelectionError:
+            print(INVALID_SELECTION_DISPLAY)
+
+
+def get_machine_menu_selection() -> MachineMenuSelection:
+    machine_menu_selection = input(PROMPT_INPUT)
+    if machine_menu_selection == "1":
+        return MachineMenuSelection.INSERT_COINS
+    if machine_menu_selection == "2":
+        return MachineMenuSelection.SELECT_WASH
+    if machine_menu_selection == "3":
+        return MachineMenuSelection.GO_BACK
+    raise InvalidMenuSelectionError
+
+
+def get_machine_menu_input() -> MachineMenuSelection:
+    while True:
+        try:
+            print(MACHINE_MENU_DISPLAY)
+            machine_menu_selection = get_machine_menu_selection()
+            return machine_menu_selection
+        except InvalidMenuSelectionError:
+            print(INVALID_SELECTION_DISPLAY)
+
+
+washing_machine_statistics = WashingMachineStatistics()
+washing_machine_state = WashingMachineState()
+
 while True:
     start_menu_input = get_start_menu_input()
     if start_menu_input == StartMenuSelection.EXIT:
         print(EXIT_DISPLAY)
         exit()
+
+    if start_menu_input == StartMenuSelection.GO_TO_MANTENANCE_MENU:
+        while True:
+            maintenance_menu_input = get_maintenance_menu_input()
+            if maintenance_menu_input == MaintenanceMenuSelection.DISPLAY_STATISTICS:
+                show_statistics(washing_machine_statistics)
+            if maintenance_menu_input == MaintenanceMenuSelection.RESET_STATISTICS:
+                washing_machine_statistics.reset()
+                print(STATISTICS_RESET_DISPLAY)
+            if maintenance_menu_input == MaintenanceMenuSelection.GO_BACK:
+                break
+
+    if start_menu_input == StartMenuSelection.GO_TO_USE_MACHINE_MENU:
+        while True:
+            machine_menu_input = get_machine_menu_input()
+            if machine_menu_input == MachineMenuSelection.INSERT_COINS:
+                ...
+            if machine_menu_input == MachineMenuSelection.SELECT_WASH:
+                ...
+            if machine_menu_input == MachineMenuSelection.GO_BACK:
+                break
