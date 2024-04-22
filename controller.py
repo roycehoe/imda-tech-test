73560c1461399010d11707a737a6b49673533589
roycehoe <royceinbox@hotmail.com>
2024-04-22 11:56:39 +0800

    feat: create MVC for maintenance menu

--- controller.py
+++ controller.py
@@ -4,17 +4,20 @@ from typing import Any
 from constants import (
     INSERT_COIN_OPTIONS_TO_COIN_VALUE_MAPPING,
     USER_INPUT_TO_INSERT_COIN_OPTIONS_MAPPING,
+    USER_INPUT_TO_MAINTENANCE_OPTIONS_MAPPING,
     USER_INPUT_TO_START_OPTIONS_MAPPING,
 )
 from display import (
+    EXIT_DISPLAY,
     INVALID_SELECTION_DISPLAY,
     MENU_INSERT_COIN_DISPLAY,
+    MENU_MAINTENANCE_DISPLAY,
     MENU_START_DISPLAY,
     PROMPT_INPUT,
     TOPUP_SUCCESS_DISPLAY,
 )
-from enums import InsertCoinOptions
-from exceptions import InvalidMenuSelectionError
+from enums import InsertCoinOptions, MaintenanceOptions, StartMenuOptions
+from exceptions import InvalidCoinValueError, InvalidMenuSelectionError
 from state import WashingMachineBalance, WashingMachineStatistics, WashSettingsMenuState
 
 
@@ -43,10 +46,6 @@ class InsertCoinMenuModel:
 
     input_mapping = USER_INPUT_TO_INSERT_COIN_OPTIONS_MAPPING
     coin_value_mapping = INSERT_COIN_OPTIONS_TO_COIN_VALUE_MAPPING
-    view = None
-
-    def __post_init__(self):
-        self.view = InsertCoinMenuView(self.state)
 
     def topup_washing_machine(self, topup_amount: float) -> None:
         self.state.balance.topup_balance(topup_amount)
@@ -62,7 +61,7 @@ class InsertCoinMenuModel:
     def get_coin_value(self, insert_coin_option: InsertCoinOptions) -> float:
         if coin_value := self.coin_value_mapping.get(insert_coin_option):
             return coin_value
-        raise Exception
+        raise InvalidCoinValueError
 
 
 @dataclass
@@ -78,7 +77,7 @@ class InsertCoinMenuView:
 
 
 @dataclass
-class InsertCoinMenuController:
+class StartMenuController:
     state: WashingMachine
 
     def __post_init__(self):
@@ -105,20 +104,116 @@ class InsertCoinMenuController:
             print(self.view.balance)
 
 
-# @dataclass
-# class StartMenuView:
-#     menu = MENU_START_DISPLAY
-#     invalid_selection = INVALID_SELECTION_DISPLAY
-#     prompt_input = PROMPT_INPUT
+@dataclass
+class StartMenuModel:
+    state: WashingMachine
 
-# @dataclass
-# class StartMenuModel:
-#     menu = MENU_START_DISPLAY
-#     invalid_selection = INVALID_SELECTION_DISPLAY
-#     prompt_input = PROMPT_INPUT
+    input_mapping = USER_INPUT_TO_START_OPTIONS_MAPPING
+
+    def change_machine_state(self, new_state):
+        self.state.change_controller(new_state)
+
+    def parse_user_input(self, user_input: str) -> StartMenuOptions:
+        if selected_option := self.input_mapping.get(user_input):
+            return selected_option
+        raise InvalidMenuSelectionError
+
+
+@dataclass
+class StartMenuView:
+    menu = MENU_START_DISPLAY
+    invalid_selection = INVALID_SELECTION_DISPLAY
+    prompt_input = PROMPT_INPUT
+    exit = EXIT_DISPLAY
+
+
+@dataclass
+class StartMenuController:
+    state: WashingMachine
+    view = StartMenuView()
+
+    def __post_init__(self):
+        self.model = StartMenuModel(state=self.state)
+
+    def run(self):
+        self.handle_user_input()
+
+    def handle_user_input(self):
+        while True:
+            print(self.view.menu)
+            user_input = input(self.view.prompt_input)
+            parsed_user_input = self.model.parse_user_input(user_input)
+
+            if parsed_user_input == StartMenuOptions.EXIT:
+                print(self.view.exit)
+                exit()
+
+            if parsed_user_input == StartMenuOptions.MANTENANCE:
+                self.state.change_controller(MaintenanceMenuController(self.state))
+
+            # if parsed_user_input == StartMenuOptions.WASH_SETTINGS:
+            #     self.state.change_controller(WashSettingsMenuState())
+
+
+@dataclass
+class MaintenanceMenuModel:
+    state: WashingMachine
+    input_mapping = USER_INPUT_TO_MAINTENANCE_OPTIONS_MAPPING
+
+    def change_machine_state(self, new_state):
+        self.state.change_controller(new_state)
+
+    def parse_user_input(self, user_input: str) -> MaintenanceOptions:
+        if selected_option := self.input_mapping.get(user_input):
+            return selected_option
+        raise InvalidMenuSelectionError
+
+    def reset_statistics(self):
+        self.state.statistics.reset()
+
+
+@dataclass
+class MaintenanceMenuView:
+    state: WashingMachine
+
+    menu = MENU_MAINTENANCE_DISPLAY
+    invalid_selection = INVALID_SELECTION_DISPLAY
+    prompt_input = PROMPT_INPUT
+    statistics = None
+
+    def __post_init__(self):
+        self.statistics = self.state.statistics
+
+
+@dataclass
+class MaintenanceMenuController:
+    state: WashingMachine
+
+    def __post_init__(self):
+        self.model = MaintenanceMenuModel(state=self.state)
+        self.view = MaintenanceMenuView(state=self.state)
+
+    def run(self):
+        self.handle_user_input()
+
+    def handle_user_input(self):
+        while True:
+            print(self.view.menu)
+            user_input = input(self.view.prompt_input)
+            parsed_user_input = self.model.parse_user_input(user_input)
+
+            if parsed_user_input == MaintenanceOptions.GO_BACK:
+                self.state.change_controller(StartMenuController(self.state))
+                break
+
+            if parsed_user_input == MaintenanceOptions.DISPLAY_STATISTICS:
+                print(self.view.statistics)
+
+            if parsed_user_input == MaintenanceOptions.RESET_STATISTICS:
+                self.model.reset_statistics()
 
 
 washing_machine = WashingMachine(WashingMachineStatistics(), WashingMachineBalance())
-controller = InsertCoinMenuController(washing_machine)
+controller = StartMenuController(washing_machine)
 washing_machine.change_controller(controller)
 washing_machine.run()
